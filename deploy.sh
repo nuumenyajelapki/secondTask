017eaa25b0f86ed1ce376ee16678be35496be970
Artyom Arnaut <artyomarnaut@MBP-Artyom.Dlink>
2020-08-21 18:11:45 +0300

    try3

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push origin -f git@github.com:nuumenyajelapki/secondTask.git master:gh-pages
+git push -f git@github.com:nuumenyajelapki/secondTask.git master:gh-pages
 
 cd -
